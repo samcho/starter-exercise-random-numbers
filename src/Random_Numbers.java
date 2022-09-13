7488e823b81bf2a8e50b870d24559c28fbd4391c
Samuel Cho <choss@wfu.edu>
2022-09-13 01:10:10 -0400

    Added some comments at the end of the file

--- src/Random_Numbers.java
+++ src/Random_Numbers.java
@@ -6,3 +6,20 @@ public class Random_Numbers {
         System.out.println("Number: " + Math.abs(r.nextInt()));
     }
 }
+
+/*
+for ((i = 0; i < 100; i++))
+do java -cp out/production/starter-exercise-random-numbers Random_Numbers
+done > rand_num.dat
+ */
+
+/*
+for i in CAT_00/*
+do
+find $i
+done > files.dat
+ */
+
+/*
+ awk '{ s+=$2 } END { printf("%d", s) }' rand_num.dat
+ */
